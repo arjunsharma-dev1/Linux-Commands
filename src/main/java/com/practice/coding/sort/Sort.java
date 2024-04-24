3699b29bbb8047c55a13b7182936d2b12e291941
Arjun Sharma <arjunsharma.dev1@gmail.com>
2024-04-24 11:04:06 +0530

    1.accepts list of files 2.added ignore leading blanks option 3.added ignore case opton 4.corrected distinct impl. with ignore case option

--- src/main/java/com/practice/coding/sort/Sort.java
+++ src/main/java/com/practice/coding/sort/Sort.java
@@ -1,20 +1,26 @@
 package com.practice.coding.sort;
 
-import picocli.CommandLine;
+import com.practice.coding.utils.Pair;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Command(name = "sort", description = "Write sorted concatenation of all FILE(s) to standard output.")
 public class Sort implements Callable<String> {
     @Parameters(index = "0",description = "file Path")
-    private String filePath;
+    private List<String> filePath;
 
     @Option(names = {"-r", "--reverse"}, description = {"reverse the result of comparisons"})
     private boolean reverse;
@@ -22,21 +28,72 @@ public class Sort implements Callable<String> {
     @Option(names = {"-u", "--unique"}, description = {"with -c, check for strict ordering; without -c, output only  the first of an equal run"})
     private boolean unique;
 
+    @Option(names = {"-b", "--ignore-leading-blanks"}, description = {"ignore leading blanks"})
+    private boolean ignoreLeadingBlanks;
+
+    @Option(names = {"-f", "--ignore-case"}, description = {"fold lower case to upper case characters"})
+    private boolean ignoreCase;
+
+    private Comparator<String> simpleOrder = String::compareTo;
+    private Comparator<String> reverseOrder = simpleOrder.reversed();
+
+    private Comparator<Pair<String, String>> pairSimpleOrder = (first, second) -> {
+        return simpleOrder.compare(first.getValue(), second.getValue());
+    };
+    private Comparator<Pair<String, String>> pairReverseOrder = pairSimpleOrder.reversed();
+
     @Override
     public String call() throws Exception {
-        var fileLines = Files.readAllLines(Paths.get(filePath));
-        Comparator<String> simpleOrder = String::compareTo;
-        Comparator<String> reverseOrder = Comparator.reverseOrder();
-
         Comparator<String> toUse = simpleOrder;
+        Comparator<Pair<String, String>> pairComparatorToUse = pairSimpleOrder;
         if (reverse) {
             toUse = reverseOrder;
+            pairComparatorToUse = pairReverseOrder;
         }
 
-        var lineStream = fileLines.stream().sorted(toUse);
-        if (unique) {
-            lineStream = lineStream.distinct();
+        var linesStream = filePath.stream().map(Paths::get).flatMap(path -> {
+            try {
+                return Files.readAllLines(path).stream();
+            } catch (IOException e) {
+                return Stream.of();
+            }
+        });
+
+        if (ignoreLeadingBlanks) {
+            linesStream = linesStream.map(String::stripLeading);
         }
-        return lineStream.collect(Collectors.joining(System.lineSeparator()));
+
+        if (ignoreCase) {
+            linesStream = linesStream.map(line -> Pair.of(line, line.toUpperCase()))
+                    .sorted(pairComparatorToUse)
+                    .map(Pair::getKey);
+        } else {
+            linesStream = linesStream.sorted(toUse);
+        }
+
+        if (unique) {
+            var distinct = new DistinctCharacter(ignoreCase);
+            linesStream = linesStream.filter(distinct);
+        }
+
+        return linesStream.collect(Collectors.joining(System.lineSeparator()));
+    }
+}
+
+class DistinctCharacter implements Predicate<String> {
+
+    private boolean ignoreCase;
+    private Set<String> uniqueSet = new HashSet<>();
+
+    DistinctCharacter(boolean ignoreCase) {
+        this.ignoreCase = ignoreCase;
+    }
+
+    @Override
+    public boolean test(String line) {
+        if (ignoreCase) {
+            line = line.toUpperCase();
+        }
+        return uniqueSet.add(line);
     }
 }
